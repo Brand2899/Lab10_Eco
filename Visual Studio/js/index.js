f2e7ca8b6a33c728be35c09a70de3d99858b25aa
Brand28 <alejandrobrand28@gmail.com>
2021-10-27 03:38:00 -0500

    muchas cosas pasaron

---
+++ Visual Studio/js/index.js
@@ -0,0 +1,38 @@
+//DECLARACIONES
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const usuario = document.getElementById('usuario');
+const correo = document.getElementById('correo');
+const contraseña = document.getElementById('contraseña');
+const contraseñaConfirmar = document.getElementById('contraseñaConfirmar');
+const bnRegistrar = document.getElementById('bnRegistrar');
+
+//FUNCIONES
+registrar = () => {
+    let n = nombre.value;
+    let a = apellido.value;
+    let u = usuario.value;
+    let co = correo.value;
+    let con = contraseña.value;
+    let ccon = contraseñaConfirmar.value;
+
+    if(con !== ccon){
+        alert('contraseñas no coinciden');
+        return;
+    }
+
+    let objetoUsuario = {
+        nombre: n,
+        apellido: a,
+        usuario: u,
+        correo: co,
+        contraseña: con
+    };
+
+    let json = JSON.stringify(objetoUsuario);
+
+    console.log(objetoUsuario);
+    console.log(json);
+}
+
+bnRegistrar.addEventListener('click', registrar);
